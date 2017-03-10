c2c4588e91377b44868a037ce9cdda327b4b70f2
Dixon Mejia-Piters (InConsulting Inc.) <v-dimeji@microsoft.com>
2017-03-09 21:04:06 -0800

    v14. Add ng-repeat functionality. Add items list

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -5,6 +5,14 @@ angular.module('blogList')
         //template: "<div class=''><h1 class='new-class'> {{ title }} </h3><button ng-click='someClickTest()'>Click here!</button></div>",
         templateUrl: '/templates/blog-list.html',
         controller: function($scope) {
+            var blogItems = [
+                {title: "Some Title", id: 1, description: "This is a book", publishDate: "2017-03-09"},
+                 {title: "Title", id: 2, description: "This is a book"},
+                  {title: "Tea", id: 3, description: "This is a book"},
+                   {title: "Lite", id: 4, description: "This is a book"},
+            ];
+
+            $scope.items = blogItems;
             console.log("hello");
             $scope.title = "Hi there";
             $scope.clicks = 0;
