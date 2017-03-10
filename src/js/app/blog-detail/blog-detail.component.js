f1c2497ffa2e9479c5d48b6907deee6367acaa2c
Dixon Mejia-Piters (InConsulting Inc.) <v-dimeji@microsoft.com>
2017-03-10 10:03:53 -0800

    Modify blog-detail.html template. Add content for specific blogItem in blog-detail.component.js. Add rerouting logic to blog-detail.component.js

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,7 +3,7 @@
 angular.module('blogDetail')
     .component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($routeParams, $scope) {
+        controller: function($location, $routeParams, $scope) {
             var blogItems = [
                 {title: "Some Title", id: 1, description: "This is a book", publishDate: "2017-03-09"},
                  {title: "Title", id: 2, description: "This is a book"},
@@ -13,5 +13,23 @@ angular.module('blogDetail')
 
             //console.log($routeParams);
             $scope.title = "Blog " + $routeParams.id;
+            $scope.notFound = true;
+            angular.forEach(blogItems, function(post){
+                if (post.id == $routeParams.id) {
+                    $scope.notFound = false;
+                    $scope.post = post;
+                }
+                console.log(post);
+            });
+
+            if($scope.notFound) {
+                console.log("Not found");
+                // change location
+                $location.path("/404"); // Just with the purpose of changing the URL.
+            }
+
+
+
+            })
         }
     });
