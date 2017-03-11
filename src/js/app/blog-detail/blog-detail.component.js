4b0bec9b0de40746bba530ec9032844ec36c02c8
Dixon Mejia-Piters (InConsulting Inc.) <v-dimeji@microsoft.com>
2017-03-10 18:51:37 -0800

    Create json folder with posts.json file (data). Add HTTP GET request of the file to blog-detail.component.js to get the data.

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -2,31 +2,53 @@
 
 angular.module('blogDetail')
     .component('blogDetail', {
-        templateUrl: 'templates/my-portfolio.html',
-        controller: function($location, $routeParams, $scope) {
-            var blogItems = [
-                {title: "Some Title", id: 1, description: "This is a book", publishDate: "2017-03-09"},
-                 {title: "Title", id: 2, description: "This is a book"},
-                  {title: "Tea", id: 3, description: "This is a book"},
-                   {title: "Lite", id: 4, description: "This is a book"},
-            ];
+        templateUrl: 'templates/blog-detail.html',
+        controller: function($http, $location, $routeParams, $scope) {
 
-            //console.log($routeParams);
-            $scope.title = "Blog " + $routeParams.id;
-            $scope.notFound = true;
-            angular.forEach(blogItems, function(post){
-                if (post.id == $routeParams.id) {
-                    $scope.notFound = false;
-                    $scope.post = post;
-                }
-                console.log(post);
-            });
+            // First parameter is the url you want to get, and the second is the headers (if any)
+
+            $http.get("/json/posts.json", {}).then(successCallback, errorCallback);
+
+            function successCallback(response, status, config, statusText) {
+                $scope.notFound = true; // Moved to be executed only after you get the data
+                console.log(response.data);
+                var blogItems = response.data;
+                $scope.posts = blogItems;
+                angular.forEach(blogItems, function(post) {
+                    if (post.id == $routeParams.id) {
+                        $scope.notFound = false;
+                        $scope.post = post;
+                    }
+                });
+            }
+
+            function errorCallback(response, status, config, statusText){
+                console.log(response);
+            }
+
 
             if($scope.notFound) {
                 console.log("Not found");
                 // change location
                 $location.path("/404"); // Just with the purpose of changing the URL.
             }
+
+            // var blogItems = [
+            //     {title: "Some Title", id: 1, description: "This is a book", publishDate: "2017-03-09"},
+            //      {title: "Title", id: 2, description: "This is a book"},
+            //       {title: "Tea", id: 3, description: "This is a book"},
+            //        {title: "Lite", id: 4, description: "This is a book"},
+            // ];
+
+            // //console.log($routeParams);
+            // $scope.title = "Blog " + $routeParams.id;
+            // angular.forEach(blogItems, function(post){
+            //     if (post.id == $routeParams.id) {
+            //         $scope.notFound = false;
+            //         $scope.post = post;
+            //     }
+            //     console.log(post);
+            // });
 
 
 
