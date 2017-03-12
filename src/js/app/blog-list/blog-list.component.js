2446c21284d4990fadd1b2a50b3febea447a2184
Dixon Mejia-Piters (InConsulting Inc.) <v-dimeji@microsoft.com>
2017-03-11 21:33:02 -0800

    Hide URL. Evaluate expressions passed as a value to an attribute (see confirm-click.directive.js). Add goToItem function to blog-list.component and use it on template

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -3,7 +3,13 @@
 angular.module('blogList')
     .component('blogList', {
         templateUrl: '/templates/blog-list.html',
-        controller: function(Post, $routeParams, $scope) {
+        controller: function(Post, $location, $routeParams, $rootScope, $scope) {
             $scope.items = Post.query();
+            $scope.goToItem = function(post) {
+                $rootScope.$apply(function() {
+                    // set the path where you want to go when this function is executed
+                    $location.path("/blog/" + post.id);
+                })
+            }
         }
     });
